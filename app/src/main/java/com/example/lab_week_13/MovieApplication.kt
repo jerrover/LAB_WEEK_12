4b0d2251ac9474abffd006dfa78608b9156e5c0c
jerrover <jerrover@gmail.com>
2025-12-28 14:04:59 +0700

    Commit No. 1 - add data binding

--- app/src/main/java/com/example/lab_week_13/MovieApplication.kt
+++ app/src/main/java/com/example/lab_week_13/MovieApplication.kt
@@ -1,4 +1,4 @@
-package com.example.test_lab_week_12
+package com.example.lab_week_13
 
 import android.app.Application
 import retrofit2.Retrofit
--- app/src/main/java/com/example/lab_week_13/MovieViewModel.kt
+++ app/src/main/java/com/example/lab_week_13/MovieViewModel.kt
@@ -1,13 +1,12 @@
-package com.example.test_lab_week_12
+package com.example.lab_week_13
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.test_lab_week_12.model.Movie
+import com.example.lab_week_13.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import java.util.Calendar
