7c78e6ce1f9aa617754945e98977245be20896a4
jerrover <jerrover@gmail.com>
2025-12-28 13:47:37 +0700

    Commit No. 3 – Assignment

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -8,13 +8,13 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import java.util.Calendar
 
 class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
 
     // define the StateFlow in replace of the LiveData
-    // a StateFlow is an observable Flow that emits state updates to the collectors
-    // MutableStateFlow is a StateFlow that you can change the value
     private val _popularMovies = MutableStateFlow(emptyList<Movie>())
     val popularMovies: StateFlow<List<Movie>> = _popularMovies
 
@@ -31,6 +31,13 @@ class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel()
         // Dispatchers.IO means that this coroutine will run on a shared pool of threads
         viewModelScope.launch(Dispatchers.IO) {
             movieRepository.fetchMovies()
+                .map { movies ->
+                    // Melakukan filter dan sorting data menggunakan operator map sebelum data di-emit ke StateFlow.
+                    val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
+                    movies.filter { movie ->
+                        movie.releaseDate?.startsWith(currentYear) == true
+                    }.sortedByDescending { it.popularity }
+                }
                 .catch {
                     // catch is a terminal operator that catches exceptions from the Flow
                     _error.value = "An exception occurred: ${it.message}"
